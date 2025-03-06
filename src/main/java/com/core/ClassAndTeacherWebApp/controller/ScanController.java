9d0ec5f99e10765508d3b7f2774e432cefeeeb99
DCoder06 <124501365+DCoder06@users.noreply.github.com>
2025-03-06 16:38:47 +0530

    Add files via upload

---
+++ src/main/java/com/core/ClassAndTeacherWebApp/controller/ScanController.java
@@ -0,0 +1,20 @@
+package com.example.QRCodeGenerator.Controllers;
+
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/scanQR")
+public class ScanController {
+
+    @PostMapping("/{merchant}")
+    public String scanCode(@PathVariable String merchant) {
+        System.out.println(merchant);
+        System.out.println("Payment done by " + "99902020932.");
+        return "Payment Successful";
+    }
+
+}
